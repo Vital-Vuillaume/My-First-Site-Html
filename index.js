dbb8fb582e371f30ee48949e753af75e3c587bde
Nuagecraft <123258530+Nuagecraft@users.noreply.github.com>
2023-04-01 22:42:25 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -4,9 +4,7 @@
 
 console.log("Bonjour la console");
 
-let Easteregg = "Bien jouer mais ce n'est que le début de l'énigme!                    indice: bouton Xbox Series X"
 
-let Vital = "Vital est beaucoup trop intelligent. il a 300 de Q1"
 
 const nav = document.querySelector('nav');
 
