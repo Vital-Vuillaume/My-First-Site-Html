70f9f39ad097bb0e1bdaab35d75f8d266dda1b16
Vital Vuillaume <123258530+Vital-Vuillaume@users.noreply.github.com>
2023-05-01 16:17:57 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//pour que le site soit sécurisé
+//pour que le site soit securise
 
 if (window.location.protocol != "https:") {
    window.location.protocol="https:";
@@ -7,22 +7,6 @@ if (window.location.protocol != "https:") {
 //pour voir si la console fonctionne
 
 console.log("Bonjour la console");
-
-//musique 
-
-var audio = document.getElementById("my-audio");
-var pauseBtn = document.getElementById("pause-btn");
-
-pauseBtn.addEventListener("click", function() {
-
-    if (audio.paused) {
-        audio.play();
-        pauseBtn.style.backgroundImage = "url(son-activer.png)";
-    } else {
-        audio.pause();
-        pauseBtn.style.backgroundImage = "url(son-désactiver.png)";
-    }
-});
 
 //barre de scroll
 
@@ -34,5 +18,21 @@ window.addEventListener('scroll',() => {
         nav.style.top = 0;
     } else {
         nav.style.top = "-80px";
+    }
+});
+
+//musique 
+
+var audio = document.getElementById("my-audio");
+var pauseBtn = document.getElementById("pause-btn");
+
+pauseBtn.addEventListener("click", function() {
+
+    if (audio.paused) {
+        audio.play();
+        pauseBtn.src="son-activer.png";
+    } else {
+        audio.pause();
+        pauseBtn.src="son-désactiver.png";
     }
 });
