ec1aab5ba163801eacb91a2221b914953c01a13f
Vital Vuillaume <123258530+Vital-Vuillaume@users.noreply.github.com>
2023-05-01 14:31:57 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -20,3 +20,18 @@ window.addEventListener('scroll',() => {
         nav.style.top = "-80px";
     }
 });
+
+//musique 
+
+var audio = document.getElementById("my-audio");
+var pauseBtn = document.getElementById("pause-btn");
+
+pauseBtn.addEventListener("click", function() {
+  if (audio.paused) {
+    audio.play();
+    pauseBtn.style.backgroundImage = "url(son-activer.png)";
+  } else {
+    audio.pause();
+    pauseBtn.style.backgroundImage = "url(son-désactiver.png)";
+  }
+})
